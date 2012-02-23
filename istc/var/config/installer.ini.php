8c960460c06758d96dcf0599931e34bc3814b11e
Charles <duroycharles@gmail.com>
2012-02-23 13:42:41 +0100

    ok projet

---
+++ istc/var/config/installer.ini.php
@@ -0,0 +1,28 @@
+;<?php die(''); ?>
+; for security reasons , don't remove or modify the first line
+; don't modify this file if you don't know what you do. it is generated automatically by jInstaller
+
+
+[index]
+jelix.installed=1
+jelix.version=1.2.6
+jacl2db.installed=0
+jacl2db.version=
+jacldb.installed=0
+jacldb.version=
+jauth.installed=0
+jauth.version=
+jauthdb.installed=0
+jauthdb.version=
+junittests.installed=0
+junittests.version=
+jWSDL.installed=0
+jWSDL.version=
+jelix.version.date="2011-10-04 17:36"
+jelix.firstversion=1.2.6
+jelix.firstversion.date="2011-10-04 17:36"
+istc.installed=1
+istc.version=0.1pre
+[__modules_data]
+jelix.contexts="db:istc"
+
